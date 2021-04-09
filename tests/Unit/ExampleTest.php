2f3e432d8252ec0530138b536f52d0cd3da0dfeb
Daniel de Wit <daniel@danieldewit.nl>
2021-04-09 12:58:58 +0200

    Add github action

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DanielDeWit\LighthouseSanctum\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_works(): void
+    {
+        $this->assertTrue(true);
+    }
+}
