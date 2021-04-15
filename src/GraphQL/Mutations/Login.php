ec7220c78149808765803726b4041b148ddf1848
wimski <wimreckman@gmail.com>
2021-04-15 17:49:12 +0200

    Apply all input validation consistently in the schema

--- src/GraphQL/Mutations/Login.php
+++ src/GraphQL/Mutations/Login.php
@@ -4,21 +4,15 @@ namespace DanielDeWit\LighthouseSanctum\GraphQL\Mutations;
 
 use Exception;
 use Illuminate\Auth\AuthManager;
-use Illuminate\Contracts\Validation\Factory as ValidationFactory;
-use Nuwave\Lighthouse\Exceptions\ValidationException;
+use Nuwave\Lighthouse\Exceptions\AuthenticationException;
 use RuntimeException;
 
 class Login
 {
-    protected ValidationFactory $validator;
     protected AuthManager $authManager;
 
-    public function __construct(
-        ValidationFactory $validator,
-        AuthManager $authManager
-    )
+    public function __construct(AuthManager $authManager)
     {
-        $this->validator = $validator;
         $this->authManager = $authManager;
     }
 
@@ -26,28 +20,20 @@ class Login
      * @param null $_
      * @param string[] $args
      * @return string[]
-     * @throws ValidationException
-     * @throws \Illuminate\Validation\ValidationException
      * @throws Exception
      */
     public function __invoke($_, array $args): array
     {
-        $validator = $this->validator
-            ->make($args, [
-                'email'    => 'required|email',
-                'password' => 'required',
-            ]);
-
         $userProvider = $this->authManager->createUserProvider(config('lighthouse-sanctum.provider'));
 
         if (! $userProvider) {
             throw new RuntimeException('No UserProvider available.');
         }
 
-        $user = $userProvider->retrieveByCredentials($validator->validated());
+        $user = $userProvider->retrieveByCredentials($args);
 
         if (! $user) {
-            throw new ValidationException('The provided credentials are incorrect.', $validator);
+            throw new AuthenticationException('The provided credentials are incorrect.');
         }
 
         if (! method_exists($user, 'createToken')) {
