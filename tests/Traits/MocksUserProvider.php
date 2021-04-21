f45afd7322381adec499a0a1781bdcbd5b132ba6
wimski <wimreckman@gmail.com>
2021-04-21 20:34:44 +0200

    Add tests for verify email mutation

---
+++ tests/Traits/MocksUserProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DanielDeWit\LighthouseSanctum\Tests\Traits;
+
+use Illuminate\Auth\AuthManager;
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Contracts\Config\Repository as Config;
+use Illuminate\Foundation\Auth\User;
+use Mockery;
+use Mockery\MockInterface;
+
+trait MocksUserProvider
+{
+    /**
+     * @return AuthManager|MockInterface
+     */
+    protected function mockAuthManager(?UserProvider $userProvider)
+    {
+        /** @var AuthManager|MockInterface $authManager */
+        $authManager = Mockery::mock(AuthManager::class)
+            ->shouldReceive('createUserProvider')
+            ->with('sanctum-provider')
+            ->andReturn($userProvider)
+            ->getMock();
+
+        return $authManager;
+    }
+
+    /**
+     * @return Config|MockInterface
+     */
+    protected function mockConfig()
+    {
+        /** @var Config|MockInterface $config */
+        $config = Mockery::mock(Config::class)
+            ->shouldReceive('get')
+            ->with('lighthouse-sanctum.provider')
+            ->andReturn('sanctum-provider')
+            ->getMock();
+
+        return $config;
+    }
+
+    /**
+     * @return UserProvider|MockInterface
+     */
+    abstract protected function mockUserProvider(?User $user);
+}
---
+++ tests/Unit/GraphQL/Mutations/VerifyEmailTest.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DanielDeWit\LighthouseSanctum\Tests\Unit\GraphQL\Mutations;
+
+use DanielDeWit\LighthouseSanctum\Contracts\Services\EmailVerificationServiceInterface;
+use DanielDeWit\LighthouseSanctum\Enums\EmailVerificationStatus;
+use DanielDeWit\LighthouseSanctum\GraphQL\Mutations\VerifyEmail;
+use DanielDeWit\LighthouseSanctum\Tests\stubs\Users\UserMustVerifyEmail;
+use DanielDeWit\LighthouseSanctum\Tests\Traits\MocksUserProvider;
+use DanielDeWit\LighthouseSanctum\Tests\Unit\AbstractUnitTest;
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Foundation\Auth\User;
+use Mockery;
+use Mockery\MockInterface;
+use RuntimeException;
+
+class VerifyEmailTest extends AbstractUnitTest
+{
+    use MocksUserProvider;
+
+    /**
+     * @test
+     */
+    public function it_verifies_an_email(): void
+    {
+        /** @var UserMustVerifyEmail|MockInterface $user */
+        $user = Mockery::mock(UserMustVerifyEmail::class)
+            ->shouldReceive('markEmailAsVerified')
+            ->getMock();
+
+        /** @var EmailVerificationServiceInterface|MockInterface $verificationService */
+        $verificationService = Mockery::mock(EmailVerificationServiceInterface::class)
+            ->shouldReceive('verify')
+            ->with($user, '1234567890')
+            ->getMock();
+
+        $userProvider = $this->mockUserProvider($user);
+
+        $mutation = new VerifyEmail(
+            $this->mockAuthManager($userProvider),
+            $this->mockConfig(),
+            $verificationService,
+        );
+
+        $result = $mutation(null, [
+            'id'   => 123,
+            'hash' => '1234567890',
+        ]);
+
+        static::assertIsArray($result);
+        static::assertCount(1, $result);
+        static::assertTrue(EmailVerificationStatus::VERIFIED()->is($result['status']));
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_an_exception_if_the_user_provider_is_not_found(): void
+    {
+        static::expectException(RuntimeException::class);
+        static::expectExceptionMessage('User provider not found.');
+
+        $mutation = new VerifyEmail(
+            $this->mockAuthManager(null),
+            $this->mockConfig(),
+            Mockery::mock(EmailVerificationServiceInterface::class),
+        );
+
+        $mutation(null, [
+            'id'   => 123,
+            'hash' => '1234567890',
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_an_exception_if_the_user_does_not_implement_the_must_verify_email_interface(): void
+    {
+        $user = Mockery::mock(User::class);
+
+        static::expectException(RuntimeException::class);
+        static::expectExceptionMessage('User must implement "Illuminate\Contracts\Auth\MustVerifyEmail".');
+
+        $userProvider = $this->mockUserProvider($user);
+
+        $mutation = new VerifyEmail(
+            $this->mockAuthManager($userProvider),
+            $this->mockConfig(),
+            Mockery::mock(EmailVerificationServiceInterface::class),
+        );
+
+        $mutation(null, [
+            'id'   => 123,
+            'hash' => '1234567890',
+        ]);
+    }
+
+    /**
+     * @return UserProvider|MockInterface
+     */
+    protected function mockUserProvider(?User $user)
+    {
+        /** @var UserProvider|MockInterface $userProvider */
+        $userProvider = Mockery::mock(UserProvider::class)
+            ->shouldReceive('retrieveById')
+            ->with(123)
+            ->andReturn($user)
+            ->getMock();
+
+        return $userProvider;
+    }
+}
