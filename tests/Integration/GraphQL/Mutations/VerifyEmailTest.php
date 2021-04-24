b03ce2b340c8eae7a98ef0c83c79c02131bf86cd
wimski <wimreckman@gmail.com>
2021-04-24 12:19:28 +0200

    Add tests for verify email input validation

--- tests/Integration/GraphQL/Mutations/VerifyEmailTest.php
+++ tests/Integration/GraphQL/Mutations/VerifyEmailTest.php
@@ -89,4 +89,70 @@ class VerifyEmailTest extends AbstractIntegrationTest
             }
         ')->assertGraphQLErrorMessage('The provided id and hash are incorrect.');
     }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_id_field_is_missing(): void
+    {
+        $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                verifyEmail(input: {
+                    hash: "foobar"
+                }) {
+                    status
+                }
+            }
+        ')->assertGraphQLErrorMessage('Field VerifyEmailInput.id of required type ID! was not provided.');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_id_field_is_not_an_id(): void
+    {
+        $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                verifyEmail(input: {
+                    id: true,
+                    hash: "foobar"
+                }) {
+                    status
+                }
+            }
+        ')->assertGraphQLErrorMessage('Field "verifyEmail" argument "input" requires type ID!, found true.');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_hash_field_is_missing(): void
+    {
+        $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                verifyEmail(input: {
+                    id: 123,
+                }) {
+                    status
+                }
+            }
+        ')->assertGraphQLErrorMessage('Field VerifyEmailInput.hash of required type String! was not provided.');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_has_field_is_not_a_string(): void
+    {
+        $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                verifyEmail(input: {
+                    id: 123,
+                    hash: 12345
+                }) {
+                    status
+                }
+            }
+        ')->assertGraphQLErrorMessage('Field "verifyEmail" argument "input" requires type String!, found 12345.');
+    }
 }
