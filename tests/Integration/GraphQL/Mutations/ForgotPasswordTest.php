afef8a1494b62b4aa98a0d4dca1445131f5e1ee4
wimski <wimreckman@gmail.com>
2021-04-24 12:30:56 +0200

    Add tests for forgot password validation

--- tests/Integration/GraphQL/Mutations/ForgotPasswordTest.php
+++ tests/Integration/GraphQL/Mutations/ForgotPasswordTest.php
@@ -49,4 +49,131 @@ class ForgotPasswordTest extends AbstractIntegrationTest
 
         Notification::assertSentTo($user, ResetPassword::class);
     }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_email_field_is_missing(): void
+    {
+        $this->graphQL(/** @lang GraphQL */'
+            mutation {
+                forgotPassword(input: {
+                    reset_password_url: {
+                      url: "https://mysite.com/reset-password?email=__EMAIL__&token=__TOKEN__"
+                    }
+                }) {
+                    status
+                    message
+                }
+            }
+        ')->assertGraphQLErrorMessage('Field ForgotPasswordInput.email of required type String! was not provided.');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_email_field_is_not_a_string(): void
+    {
+        $this->graphQL(/** @lang GraphQL */'
+            mutation {
+                forgotPassword(input: {
+                    email: 12345
+                    reset_password_url: {
+                      url: "https://mysite.com/reset-password?email=__EMAIL__&token=__TOKEN__"
+                    }
+                }) {
+                    status
+                    message
+                }
+            }
+        ')->assertGraphQLErrorMessage('Field "forgotPassword" argument "input" requires type String!, found 12345.');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_email_field_is_not_an_email(): void
+    {
+        $this->graphQL(/** @lang GraphQL */'
+            mutation {
+                forgotPassword(input: {
+                    email: "foobar"
+                    reset_password_url: {
+                      url: "https://mysite.com/reset-password?email=__EMAIL__&token=__TOKEN__"
+                    }
+                }) {
+                    status
+                    message
+                }
+            }
+        ')
+            ->assertGraphQLErrorMessage('Validation failed for the field [forgotPassword].')
+            ->assertGraphQLValidationError(
+                'input.email',
+                'The input.email must be a valid email address.',
+            );
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_reset_password_url_field_is_missing(): void
+    {
+        $this->graphQL(/** @lang GraphQL */'
+            mutation {
+                forgotPassword(input: {
+                    email: "foo@bar.com"
+                    reset_password_url: {}
+                }) {
+                    status
+                    message
+                }
+            }
+        ')->assertGraphQLErrorMessage('Field ResetPasswordUrlInput.url of required type String! was not provided.');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_reset_password_url_field_is_not_a_string(): void
+    {
+        $this->graphQL(/** @lang GraphQL */'
+            mutation {
+                forgotPassword(input: {
+                    email: "foo@bar.com"
+                    reset_password_url: {
+                      url: 12345
+                    }
+                }) {
+                    status
+                    message
+                }
+            }
+        ')->assertGraphQLErrorMessage('Field "forgotPassword" argument "input" requires type String!, found 12345.');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_an_error_if_the_reset_password_url_field_is_not_a_url(): void
+    {
+        $this->graphQL(/** @lang GraphQL */'
+            mutation {
+                forgotPassword(input: {
+                    email: "foo@bar.com"
+                    reset_password_url: {
+                      url: "google"
+                    }
+                }) {
+                    status
+                    message
+                }
+            }
+        ')
+            ->assertGraphQLErrorMessage('Validation failed for the field [forgotPassword].')
+            ->assertGraphQLValidationError(
+                'input.reset_password_url.url',
+                'The input.reset password url.url format is invalid.',
+            );
+    }
 }
