8f25842fcb2364fd6bbe12011686ce24674c6146
Daniël de Wit <daniel@danieldewit.nl>
2022-06-26 10:46:17 +0200

    Add Integration test for custom user identification (#82)

---
+++ tests/stubs/Users/UserHasApiTokensIdentifiedByUsername.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DanielDeWit\LighthouseSanctum\Tests\stubs\Users;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\Contracts\HasApiTokens as HasApiTokensContract;
+use Laravel\Sanctum\HasApiTokens;
+
+class UserHasApiTokensIdentifiedByUsername extends User implements HasApiTokensContract
+{
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+
+    /**
+     * @var string
+     */
+    protected $table = 'users_with_usernames';
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+        'email_verified_at',
+    ];
+
+    /**
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * @return UserHasApiTokensIdentifiedByUsernameFactory
+     */
+    protected static function newFactory(): Factory
+    {
+        return new UserHasApiTokensIdentifiedByUsernameFactory();
+    }
+}
---
+++ tests/stubs/migrations/create_users_with_username_table.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::create('users_with_usernames', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->string('name');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+};
